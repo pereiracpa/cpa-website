71de3a63543fd8b1f61eab0d97413a1b2742177c
mlinski <mlinski@daisyintel.com>
2026-05-17 21:56:16 -0400

    change of EmailJs service id

--- script.js
+++ script.js
@@ -122,7 +122,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 submitButton.textContent = 'Sending...';
                 submitButton.disabled = true;
 
-                emailjs.sendForm('service_vefw45r', 'template_nagqr46', consultationForm)
+                //emailjs.sendForm('service_vefw45r', 'template_nagqr46', consultationForm)
+                emailjs.sendForm('service_x8kjkwk', 'template_nagqr46', consultationForm)
                     .then(() => {
                         submitButton.textContent = 'Thank You! We\'ll Contact You Soon';
                         submitButton.style.backgroundColor = '#48bb78';
